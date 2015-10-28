0108408c6ecccc5212afa3cc37ccc9d9227d82aa
yashwanthkatta <yashwanth.katta@gmail.com>
2015-10-27 20:34:42 -0500

    initial commit

---
+++ Button_Demo/Button_Demo/buttondirective.js
@@ -0,0 +1,29 @@
+﻿
+var myModule = angular.module('myModule', []);
+
+myModule.controller('Ctrl', ['$scope', function ($scope) {
+
+    
+        $scope.abc = "aaaaaaa";
+   
+}]);
+
+//Directive Here
+
+myModule.directive('onoff', function () {
+    return {
+        restrict: 'A',
+        template: '<input value="" class="btn btn-success btn-lg">',
+        link: function (scope, elem, attrs) {
+            elem.bind("click", function () {
+                console.log('onoff clicked', elem)
+                if (elem.val() == "on") {
+                    elem.val("off");
+                } else {
+                    elem.val("on");
+                }
+            })
+        }
+    }
+
+});
